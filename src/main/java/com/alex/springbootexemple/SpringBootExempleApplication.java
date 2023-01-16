af3547b993552061817660a71e7d644065730b8e
alexbalak21 <alex.balak@outlook.com>
2023-01-16 17:13:48 +0100

    Initial

---
+++ src/main/java/com/alex/springbootexemple/SpringBootExempleApplication.java
@@ -0,0 +1,13 @@
+package com.alex.springbootexemple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExempleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExempleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex/springbootexemple/SpringBootExempleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.springbootexemple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExempleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
